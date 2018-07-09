562b95635169c13ca1f8fe21184e4c808ca08cb5
Damir Handoyo <koko@damirs-mbp.damr.biz>
2018-07-09 10:22:22 +0700

    update tap dance

---
+++ keyboards/ergodox_ez/keymaps/damir/zzz2496.h
@@ -0,0 +1,33 @@
+#ifndef zzz2496
+#define zzz2496
+
+#include "../../../../quantum/quantum.h"
+#include "../../../../quantum/process_keycode/process_tap_dance.h"
+
+
+typedef struct {
+  bool is_press_action;
+  int state;
+} xtap;
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4,
+  DOUBLE_SINGLE_TAP = 5, //send two single taps
+  TRIPLE_TAP = 6,
+  TRIPLE_HOLD = 7
+};
+
+//Tap dance enums
+enum {
+    CTL_X = 0,
+    SOME_OTHER_DANCE
+}
+
+int cur_dance (qk_tap_dance_state_t *state);
+
+//for the x tap dance. Put it here so it can be used in any keymap
+void x_finished (qk_tap_dance_state_t *state, void *user_data);
+void x_reset (qk_tap_dance_state_t *state, void *user_data);
